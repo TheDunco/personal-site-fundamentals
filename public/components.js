0514443176d550c2ddb172c61592c52f9f054b8a
Duncan Van Keulen <duncvan@gmail.com>
2026-04-27 09:50:44 -0400

    Content and broader styles

--- public/components.js
+++ public/components.js
@@ -2,18 +2,19 @@
 
 class NavComponent extends HTMLElement {
     connectedCallback() {
-        this.innerHTML = `<nav class="w-full flex flex-row gap-2 border-b border-gray-500">
+        this.innerHTML = `<nav class="w-full ps-3 flex bg-white/10 backdrop-blur-md flex-row gap-2 border-b border-gray-500">
       <a href="/">Home</a>
       <a href="/work">Work</a>
       <a href="/play">Play</a>
     </nav>`
+        this.classList = `${this.classList} sticky top-0`
     }
 }
 customElements.define('x-nav', NavComponent);
 
 class FooterComponent extends HTMLElement {
     connectedCallback() {
-        this.innerHTML = `<footer class="text-sm text-gray-700 absolute bottom-0 w-full">
+        this.innerHTML = `<footer class="text-sm text-gray-700 bg-white sticky bottom-0 py-2 mt-auto w-screen">
       &#169; Duncan Van Keulen ${new Date().getFullYear()}
     </footer>`
     }
